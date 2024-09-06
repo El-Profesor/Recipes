6bbf2d030bf64c6171068c07d4cacbb080529e71
Nicolas Brousse <n.brousse@gmail.com>
2024-09-06 07:33:38 +0200

    Implement RecipeIngredients removal on Recipe update

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Recipe;
 use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -53,10 +54,34 @@ final class RecipeController extends AbstractController
     #[Route('/{id}/edit', name: 'app_recipe_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Recipe $recipe, EntityManagerInterface $entityManager): Response
     {
+        $originalRecipeIngredients = new ArrayCollection();
+
+        // Create an ArrayCollection of the current RecipeIngredient objects in the database
+        foreach ($recipe->getRecipeIngredients() as $recipeIngredient) {
+            $originalRecipeIngredients->add($recipeIngredient);
+        }
+
         $form = $this->createForm(RecipeType::class, $recipe);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Remove the relationship between the RecipeIngredient and the Recipe
+            foreach ($originalRecipeIngredients as $recipeIngredient) {
+                if (false === $recipe->getRecipeIngredients()->contains($recipeIngredient)) {
+                    // Remove the RecipeIngredient from the Recipe
+
+                    $recipe->getRecipeIngredients()->removeElement($recipeIngredient);
+                    // If it was a many-to-one relationship, remove the relationship like this
+                    $recipeIngredient->setRecipe(null);
+
+                    $entityManager->persist($recipeIngredient);
+
+                    // If you wanted to delete the Tag entirely, you can also do that
+                    $entityManager->remove($recipeIngredient);
+                }
+            }
+
+            $entityManager->persist($recipe);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_recipe_index', [], Response::HTTP_SEE_OTHER);
